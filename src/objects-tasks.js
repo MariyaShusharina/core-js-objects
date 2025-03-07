2c1a1e1cd641346a4906b1397eeab169d1e16d24
Mariya Shusharina <mariya.shushar@gmail.com>
2025-03-07 12:40:02 +0300

    feat: do task 7

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -139,8 +139,19 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  let word = [];
+  word = [];
+  let entries = [];
+  entries = Object.entries(lettersObject);
+  for (let i = 0; i < entries.length; i += 1) {
+    for (let j = 0; j < entries[i][1].length; j += 1) {
+      const letterIndex = entries[i][1][j];
+      const letter = entries[i][0];
+      word[letterIndex] = letter;
+    }
+  }
+  return word.join('');
 }
 
 /**
